18a88d02c794b550a17d9329e889fd9ae6eb5e65
damerlan17 <uchunakdamerlan@gmail.com>
2025-10-17 11:36:47 +0700

    2 comitt

--- django_posts/posts/views.py
+++ django_posts/posts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    data = {"header": "helo world", "message": "urea in django"}
-    return render(request, 'index.html', context=data)
+    return render(request, 'index.html', context={"person": Person("Jon")})
 
-# Create your views here.
+class Person:
+
+    def __init__(self, name):
+        self.name = name
