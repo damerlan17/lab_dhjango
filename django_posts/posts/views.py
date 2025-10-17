666092d13d824037b1119f5f4f317cebcb45086b
damerlan17 <uchunakdamelan@gmail/com>
2025-10-17 19:07:25 +0700

    fix base.html

--- django_posts/posts/views.py
+++ django_posts/posts/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html', context={"person": Person("Jon")})
+    return render(request, 'index.html')
 
-class Person:
-
-    def __init__(self, name):
-        self.name = name
+def post_list(request):
+    return render(request, 'post_list.html')
