a66f539e906063cba3ac47920d4f8e3b13fc550e
damerlan17 <uchunakdamelan@gmail/com>
2025-10-18 16:55:37 +0700

    починил css

--- django_posts/posts/views.py
+++ django_posts/posts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return render(request, 'index.html')
-
-def post_list(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts":posts})
